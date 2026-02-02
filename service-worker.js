5f8760597abd74918cede72c9cbc4ab0f66558c8
shahinsir <alamshahinur242@gmail.com>
2026-02-02 14:37:53 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'madrasa-management-v1.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;600;700&display=swap',
+  'https://i.ibb.co.com/hhVX9hp/android-chrome-192x192.png',
+  'https://i.ibb.co.com/gb91ZXh7/android-chrome-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
